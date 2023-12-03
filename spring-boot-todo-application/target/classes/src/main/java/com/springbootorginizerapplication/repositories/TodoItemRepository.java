3ea446dd2d19c94b00e0a12d8d03e7f1c7643086
FedorenkoYelyzaveta12 <152794084+FedorenkoYelyzaveta12@users.noreply.github.com>
2023-12-03 17:49:19 +0200

    Delete spring-boot-todo-application/target/classes directory (changing a structure)

--- spring-boot-todo-application/target/classes/src/main/java/com/springbootorginizerapplication/repositories/TodoItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.springbootorginizerapplication.repositories;
-
-import com.springbootorginizerapplication.models.TodoItem;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TodoItemRepository extends CrudRepository < TodoItem, Long> {
-
-}
